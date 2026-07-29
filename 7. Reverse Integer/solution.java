bea77d77f6d1a00c7d2127a36ddbbd492161da35
Aditya Raj <83997534+adityaraj607@users.noreply.github.com>
2026-07-29 22:25:48 +0530

    Implement reverse integer function7

---
+++ 7. Reverse Integer/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        long num = 0;
+        while (x != 0) {
+            int last = x % 10;
+            num = num * 10 + last;
+            x = x / 10;
+        }
+        if (num < Integer.MIN_VALUE || num > Integer.MAX_VALUE) {
+            return 0;
+        }
+        return (int) num;
+    }
+}

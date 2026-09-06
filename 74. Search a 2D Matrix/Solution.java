46b49bbd4d963ce0d98b04067a388a165d42db70
Aditya Raj <raj.aditya5030@gmail.com>
2026-09-06 12:31:58 +0530

    74

---
+++ 74. Search a 2D Matrix/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int size=matrix.length;
+        for (int i=0;i<size;i++){
+            for (int j=0;j<size;j++){
+                if (matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
